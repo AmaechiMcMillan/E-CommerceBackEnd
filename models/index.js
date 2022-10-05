f4dd7d39f7cbfa1accd30ee06f7eb86cb82f5838
Amaechi McMillan <amaechimcmillan@gmail.com>
2022-10-05 14:17:59 -0500

    updated files

--- models/index.js
+++ models/index.js
@@ -5,7 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Products.belongsToMany(Category, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Trip,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'planned_trips'
+});
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
